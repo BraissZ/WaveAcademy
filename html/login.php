21706a98b732529708f18946fbe27e185d52b422
ifvenas1 <202015210071@ifba.edu.br>
2023-05-26 23:02:54 -0300

    Login
    Página de Login do Site.

---
+++ html/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <div class="cabecalho-logo">
+        <img class="imagem-logo" src="imagens-index/imagem-logo.png" alt="">
+        <h1 class="titulo-logo">WaveAcademy</h1>
+        </div>
+        <h4 class="cabecalho-opcoes">Categorias</h4>
+        <div class="barra-pesquisa">
+            <input type="text" placeholder="Pesquise por qualquer coisa">
+        </div>
+        <h4 class ="cabecalho-opcoes">Nossos professores</h4>
+        <h4 class ="cabecalho-opcoes">Ensine na Udemy</h4>
+        <img src="imagens-index/carrinho.png" alt="">
+        <div class="botoes">
+            <button class="botao-cadastrar">Cadastre-se</button>
+        </div>
+        <button class="botao-web"><img src="imagens-index/web.png" alt=""></button>
+
+    </header>
+    <main class="conteudo">
+    <section class="conteudo-esquerda">
+    <style>svg#freepik_stories-thesis:not(.animated) .animable {opacity: 0;}svg#freepik_stories-thesis.animated #freepik--Books--inject-357 {animation: 3s 1 forwards ease-in fadeIn,1.5s Infinite  linear floating;animation-delay: 0s,3s;}svg#freepik_stories-thesis.animated #freepik--Character--inject-357 {animation: 1.5s Infinite  linear floating;animation-delay: 0s;}            @keyframes fadeIn {                0% {                    opacity: 0;                }                100% {                    opacity: 1;                }            }                    @keyframes floating {                0% {                    opacity: 1;                    transform: translateY(0px);                }                50% {                    transform: translateY(-10px);                }                100% {                    opacity: 1;                    transform: translateY(0px);                }            }        </style>
+    <img src="imagens-index/imagem-login.svg" alt="">
+    </section>
+    <section  class ="conteudo-direita">  
+    <div class="login">
+        <label class="titulo-login" >Efetue o Login</label>
+        <input class="input-email" type="email" name="email" id="email" placeholder="email">
+        <input class= "input-senha"type="password" name="senha" id="senha"  placeholder="senha">
+        <a href="index.php"><button class="botao-login2">LOGIN</button></a>
+       </div>
+       </section> 
+
+
+    </main>
+    <footer class="rodape">
+        <div class="links">
+        <a class="links-rodape" href="">CodeWave Businnes</a>
+        <a class="links-rodape" href="">Vire professor na CodeWave</a>
+        <a class="links-rodape" href="">Quem somos</a>
+        <a class="links-rodape"href="">Fale conosco</a>
+        </div>
+        <div class="logo-rodape">
+            <img src="imagens-index/logo-rodape.png" alt="">
+            <h4><img src="imagens-index/copyright.png" alt="">2023, WaveAcademy inc.</h4>
+        </div>
+
+
+    </footer>
+    
+</body>
+</html>
