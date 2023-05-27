315dc3591bf6f7cacad30c0cd8fc6c2d4db420de
Wendel dos Santos Costa <luaragabrielleguimaraesaraujo@gmail.com>
2023-05-26 23:16:06 -0300

    Arquivo PHP dados.php
    Arquivo com os dados do nosso site

---
+++ php/dados.php
@@ -0,0 +1,106 @@
+
+<?php
+
+    $cursos = [   
+        [        
+            "nome" => "Primeiros Socorros",
+            "fotoprofessor" => "imagens-curso\professor1.png",
+            "foto" => "imagens-curso/anna.png", 
+            "prof" => "Anna Luisa",
+            "descricao" => "O curso de primeiros socorros prepara para os incidentes da vida, lhe preparando para qualquer momento inoportuno"
+        ],
+
+        [       
+            "nome" => "Dança",
+            "fotoprofessor" => "imagens-curso\danca.png", 
+            "foto" => "imagens-curso/antonio.png",    
+            "prof" => "Antônio Danças",
+            "descricao" => "O nosso magistral curso de dança. Dance como um bailarino de verdade. Curso ministrado pelo nosso querido e, melhor dançarino de todos os tempos, DANÇA!"
+        ],
+
+        [  
+            "nome" => "Violão - Do Básico Ao Avançado",
+            "fotoprofessor" => "imagens-curso\silvioeviolao.png", 
+            "foto" => "imagens-curso/silvio.png",     
+            "prof" => "Silvio Júnior",
+            "descricao" => "Aprenda a tocar violão como ninguém, ou melhor, como Sílvio, tocador da banda TADEU E TADANDO."
+        ],
+
+        [  
+            "nome" => "Cantadas - Como Paquerar versão atualizada 2.0",
+            "fotoprofessor" => "imagens-curso\chatsmulekecantada.png",
+            "foto" => "imagens-curso/tats.png",      
+            "prof" => "Pellé",
+            "descricao" => "Quer aprender a ser mulhersexual como esse homem exuberante? Faça esse curso e saiba como conquistar uma mulher."
+        ],
+
+        [   
+            "nome" => "Como Desenvolver Liderança",
+            "fotoprofessor" => "imagens-curso\kaiopresidente.png",
+            "foto" => "imagens-curso/kaio.png",        
+            "prof" => "Presidente Obama",
+            "descricao" => "Nesse curso você aprenderá a ser um bom líder. Comande sua equipe, seu grupo, sua vida, seja lá o que for. Só garantimos que sairá sabendo como liderar!"
+        ],
+
+        [   
+            "nome" => "Microscopia - Entenda o Microscópio Óptico",
+            "fotoprofessor" => "imagens-curso\laisemicroscopio.png",
+            "foto" => "imagens-curso/lais.png",        
+            "prof" => "Dra Laís Caldas",
+            "descricao" => "Utilize o microscópio como ninguém. Após esse curso manuseie com imenso conhecimento e destreza!"
+        ],
+
+        [   
+            "nome" => "Desenvolva Oratória",
+            "fotoprofessor" => "imagens-curso\gutoorador.png",
+            "foto" => "imagens-curso/gustavo.png",        
+            "prof" => "Gustavo Monteiro",
+            "descricao" => "Aprenda os melhores métodos para ser um bom orador. Seja mestre em palestra e difunda seu conhecimento, palestrante!"
+        ],
+
+        [   
+            "nome" => "Banco de Dados - Domine o MYSQL",
+            "fotoprofessor" => "imagens-curso\luarabd.png",
+            "foto" => "imagens-curso/luara.png",      
+            "prof" => "Luara Dízio",
+            "descricao" => "Banco de dados hoje está presente em todos os lugares. Neste curso você terá acesso a um diagrama de classes que se tornará um banco de dados no MYSQL, podendo armazenar dados de um minimundo."
+        ],
+
+        [   
+            "nome" => "Aprenda a Plantar Cannabis",
+            "fotoprofessor" => "imagens-curso\claraplantand.png", 
+            "foto" => "imagens-curso/clara.png",    
+            "prof" => "Clara Guedes",
+            "descricao" => "Plante cannabis, sem a polícia saber, ilegalmente. Se o seu patrão não deixa você apertar a braba, demita o seu patrão!"
+        ],
+
+        [   
+            "nome" => "Como escrever Cordel - Conceitos intermediários",
+            "fotoprofessor" => "imagens-curso\zelendocordel.png", 
+            "foto" => "imagens-curso/ze.png",    
+            "prof" => "Zé Vinicius",
+            "descricao" => "Escrever cordel vai ser extremamente simples e lindo. Faça cordéis exuberantes, e declame nas praças públicas."
+        ],
+
+        [   
+            "nome" => "Configure Mesa de Som - Com Projetos Práticos",
+            "fotoprofessor" => "imagens-curso\abnereludymesa.png",
+            "foto" => "imagens-curso/abnereludymesa.png", 
+            "prof" => "Abner & Ludy",
+            "descricao" => "Saiba mexer com mesas de som. Seja o novo DJ de todas as festas!"
+        ],
+
+        [   
+            "nome" => "Monte sua Padaria - Faça Pães",
+            "fotoprofessor" => "imagens-curso\dandarapaes.png",
+            "foto" => "imagens-curso/dandara.png", 
+            "prof" => "Dands Cilhuda",
+            "descricao" => "Faça pães como um verdadeiro francês. Faça cacetinho, pão de sal, pão de doce e pão de mel."
+        ],
+    ]; 
+    
+
+    
+?>	
+
+
