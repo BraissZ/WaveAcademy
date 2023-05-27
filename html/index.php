bfb378dbaf99b089f77ea08abdda33fba7873b14
ifvenas1 <202015210071@ifba.edu.br>
2023-05-26 22:59:49 -0300

    Criação do HTML
    Página HTML index do site.

---
+++ html/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap');
+    </style>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap');
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <div class="cabecalho-logo">
+        <img class="imagem-logo" src="imagens-index/imagem-logo.png" alt="">
+        <h1 class="titulo-logo">WaveAcademy</h1>
+        </div>
+        <h4 class="cabecalho-opcoes">Categorias</h4>
+        <div class="barra-pesquisa">
+            <img class="search-icon" src="imagens-index/search.png" alt="">
+            <input class="search-text" type="text" placeholder="Pesquise por qualquer coisa">
+        </div>   
+        <h4 class ="cabecalho-opcoes">Nossos professores</h4>
+        <h4 class ="cabecalho-opcoes">Ensine na Udemy</h4>
+        <img src="imagens-index/carrinho.png" alt="">
+        <div class="botoes">
+            <a href="login.php"><button  class="botao-login">Login</button></a>
+            <button class="botao-cadastre-se">Cadastre-se</button>
+        </div>
+        <button class="botao-web"><img src="imagens-index/web.png" alt=""></button>
+
+    </header>
+    <main class="conteudo">
+        <div class="primeiro-conteudo">
+            <div class="primeiro-conteudo-esquerda">
+                <h2>Seja como a água e adquira forma em <br> sua VIDA!</h2>
+                <h4>Cursos a partir de 24,99. Aproveite a oferta para novos assinantes antes que expire</h4>
+
+            </div>
+            <div class="primeiro-conteudo-direita">
+                <img src="imagens-index/imagem-principal.png" alt="">
+            </div>
+        </div>
+        <div class="segundo-conteudo">
+            <h1 class="titulo-segundo-conteudo">Uma ampla seleção de cursos</h1>
+            <h3 class="subtitulo-segundo-conteudo">Escolha entre 213.000 cursos em vídeo online com novas adições publicadas mensalmente</h3>
+            <div class="descricao-cursos">
+                <h1 class="titulo-descricao">Amplie suas oportunidades de carreira com nossos cursos</h1>
+                <p>Bem-vindo(a) ao nosso site, o lugar perfeito para expandir seus horizontes! Aqui você encontrará uma variedade de cursos envolventes e relevantes, projetados para impulsionar sua carreira e enriquecer sua vida. Não perca tempo, explore nossas opções de cursos e comece sua jornada de aprendizado hoje mesmo!</p>
+                <button><a href="paginadecursos.php">Explorar Cursos</a></button>
+            </div>
+            <div class="rodape-segundo-conteudo">
+                <div class="propaganda1">
+                    <img src="imagens-index/play_icon.png" alt="">
+                    <p>Aprenda as habilidades mais <br> cobiçadas com os melhores <br> vídeos.</p>
+                </div>
+                <div class="propaganda2">
+                    <img src="imagens-index/star_icon.png" alt="">
+                    <p>Desfrute de cursos ministrados <br> por verdadeiros especialistas no <br> assunto.</p>
+                </div>
+                <div class="propaganda3">
+                    <img src="imagens-index/infinity_icon.png" alt="">
+                    <p>Aprenda no seu próprio ritmo com <br> acesso vitalício em dispositivos <br> móveis e computadores.</p>
+                </div>
+            </div>
+        </div>
+        <div class="terceiro-conteudo">
+            <h1 class="titulo-terceiro-conteudo">
+                Principais Categorias:
+            </h1>
+            <div class="categorias">
+            <div class="catergoria1">
+                <img class="imagem-categoria1" src="imagens-index/categoria1.png" alt="">
+                <h3>Desenvolvimento <br> Profissional</h3>
+            </div>
+            <div class="catergoria2">
+                <img class="imagem-categoria2" src="imagens-index/categoria2.png" alt="">
+                <h3>TI e Software</h3>
+            </div>
+             <div class="catergoria3">
+                <img class="imagem-categoria3" src="imagens-index/categoria3.png" alt="">
+                <h3>Dança e Musica</h3>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+    <footer class="rodape">
+        <div class="links">
+        <a class="links-rodape1" href="">CodeWave Businnes</a>
+        <a class="links-rodape" href="">Vire professor na CodeWave</a>
+        <a class="links-rodape" href="">Quem somos</a>
+        <a class="links-rodape4" href="">Fale conosco</a>
+        </div>
+        <div class="wave-logo">
+            <img src="imagens-index/logo-rodape.png" alt="">
+            <h1 class="titulo-rodape">WaveAcademy</h1>
+        </div>
+        <div class="copyright-logo">
+            <img src="imagens-index/copyright.png" alt="">
+            <h4>2023, WaveAcademy inc.</h4>
+        </div>
+
+    </footer>
+    
+</body>
+</html>
